4659c4782c1a7443dc1b739389e7f9d2b7afd67b
KevinMak2308 <kevi4196@stud.kea.dk>
2021-10-13 14:23:03 +0200

    Second

--- js/CreateMovie.js
+++ js/CreateMovie.js
@@ -1,19 +1,32 @@
 const out = (str) => console.log(str);
 
-document.addEventListener("DOMContentLoaded", createFormEventListener);
+function newMovie() {
 
-let movieForm;
 
-function createFormEventListener(){
 
-  movieForm = document.getElementById("newMovieForm");
-  out("movieForm" + movieForm)
-  movieForm.addEventListener("submit", handleFormSubmit);
-}
+  let category = document.getElementById("inpCat").value
+  let title = document.getElementById("inpTitle").value
+  let duration = document.getElementById("inpDuration").value
+  let age = document.getElementById("inpAge").value
 
-let a = document.getElementById(".inpTitle")
-out(a);
 
-async function handleFormSubmit(event){
-  //const form = event.
+  let postMovieRequest = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      "movieCategory": category,
+      "movieTitle": title,
+      "movieDuration": duration,
+      "movieAgeRestriction": age,
+
+    })
+
+  }
+  fetch("http://localhost:8080/createMovie", postMovieRequest)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
 }
