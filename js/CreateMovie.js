451d44f76490f7faecdda1e5edde75f31cfd6084
Snimb <72379945+Snimb@users.noreply.github.com>
2021-10-13 12:24:02 +0200

    1st

--- js/CreateMovie.js
+++ js/CreateMovie.js
@@ -1,3 +1,19 @@
 const out = (str) => console.log(str);
 
+document.addEventListener("DOMContentLoaded", createFormEventListener);
 
+let movieForm;
+
+function createFormEventListener(){
+
+  movieForm = document.getElementById("newMovieForm");
+  out("movieForm" + movieForm)
+  movieForm.addEventListener("submit", handleFormSubmit);
+}
+
+let a = document.getElementById(".inpTitle")
+out(a);
+
+async function handleFormSubmit(event){
+  //const form = event.
+}
