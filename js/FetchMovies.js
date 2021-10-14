7f2a1126438c1ab2b6f3751f18f7e064ec86225f
OliverChrestensen <oliver.chrestensen@hotmail.com>
2021-10-14 14:22:36 +0200

    .

--- js/FetchMovies.js
+++ js/FetchMovies.js
@@ -7,19 +7,43 @@ function fetchMoviesData() {
     .then(appendData)
 }
 
+const table = document.getElementById("showAllMovies");
+
 function appendData(data) {
-  var mainContainer = document.getElementById("showAllMovies");
-  for (var i = 0; i < data.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = 'MovieName: ' + data[i].movieTitle + ' Movie Duration ' + data[i].movieDuration
+  for (let i = 0; i < data.length; i++) {
+    const movie = data[i];
+    let rowCount = table.rows.length;
+    let row = table.insertRow(rowCount);
+    let cell1 = row.insertCell(0);
+    cell1.innerHTML = movie.movieTitle;
+    let cell2 = row.insertCell(1);
+    cell2.innerHTML = movie.movieDuration;
+    let cell3 = row.insertCell(2);
+    cell2.innerHTML = movie.moviec;
 
-
-    mainContainer.appendChild(div);
   }
 }
 
 
+/*function addRow(movie) {
+  let rowCount = table.rows.length;
+  let row = table.insertRow(rowCount);
+  row.id = movie.movieTitle;
+
+  let cell1 = row.insertCell(0);
+  cell1.innerHTML = movie.movieTitle;
+
+  let cell2 = row.insertCell(1);
+  let p = document.createElement("p");
+  p.innerHTML = movie.movieTitle;
+  cell2.appendChild(p);
+}
+
+ */
+
 appendData(fetchMoviesData())
+
+
 
 
 
