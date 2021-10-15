3843c6c37c5a8b1a0f013bbab624403ea01444e3
Snimb <72379945+Snimb@users.noreply.github.com>
2021-10-15 11:07:38 +0200

    2nd

--- js/FetchMovies.js
+++ js/FetchMovies.js
@@ -10,12 +10,14 @@ function fetchMoviesData() {
 const table = document.getElementById("showAllMovies");
 
 function appendData(data) {
+
   for (let i = 0; i < data.length; i++) {
     const movie = data[i];
     let rowCount = table.rows.length;
     let row = table.insertRow(rowCount);
     let cell1 = row.insertCell(0);
     cell1.innerHTML = movie.movieTitle;
+
     let cell2 = row.insertCell(1);
     cell2.innerHTML = movie.movieDuration;
     let cell3 = row.insertCell(2);
@@ -25,12 +27,72 @@ function appendData(data) {
     let cell5 = row.insertCell(4);
     cell5.innerHTML = movie.movieActors;
 
+    let cell6 = row.insertCell(5);
+    const editbutton = document.createElement("input");
+    editbutton.type = "button";
+    editbutton.setAttribute("value", "Edit");
+    cell6.appendChild(editbutton);
+    let cell7 = row.insertCell(6);
+    const updatebutton = document.createElement("input");
+    updatebutton.type ="button";
+    updatebutton.setAttribute("value", "Update")
+    cell7 = row.appendChild(updatebutton);
+    let cell8 = row.insertCell(7);
+    const deletebutton = document.createElement("input");
+    deletebutton.type ="button";
+    deletebutton.setAttribute("value", "Delete")
+    cell7 = row.appendChild(deletebutton);
+
+    editbutton.onclick = function () {
+
+           }
+
+    updatebutton.onclick = function () {
+
+    }
+
+    deletebutton.onclick = function () {
+
+    }
+
+    }
   }
-}
 
+  async function updateMovie(movie) {
+    try {
+      const response = await restUpdateMovie(movie);
+      out(response);
 
-appendData(fetchMoviesData())
+    } catch (error) {
+      alert(error.message);
+      out(error);
+    }
 
+  }
+
+  async function restUpdateMovie(movie) {
+    const updateURL = "http://localhost:8080/updateMovie/" + movie.id
+    const jsonString = JSON.stringify(movie);
+    out(jsonString);
+
+    const fetchOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonString
+    }
+    const response = await fetch(updateURL, fetchOptions);
+    out("Vi har rettet");
+    out(response.status);
+    out(response.ok);
+    if (!response.ok) {
+      out("det gik ikke godt");
+    }
+    return response.json();
+  }
+
+    appendData(fetchMoviesData());
 
 
 
